9ad15020f2541fcf3edd089156ac2922d36ff227
Tobias Rishøj <tobias_rishoej@hotmail.com>
2017-10-02 19:19:35 +0200

    update

--- app/actions/oauth.js
+++ app/actions/oauth.js
@@ -5,7 +5,7 @@ import cookie from 'react-cookie';
 import { browserHistory } from 'react-router';
 
 let domain = 'http://localhost:3000';
-if(!location.hostname === "localhost"){
+if(!location.hostname !== 'localhost'){
   domain = 'https://morning-cliffs-15123.herokuapp.com';
 }
 // Sign in with Facebook
